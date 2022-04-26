65c839f35650b1156747a72bd8de693396e76e80
Aniket Gaikwad <aniketg721@gmail.com>
2022-04-26 22:31:39 +0530

    default change

---
+++ 25 April JS intro/script.js
@@ -0,0 +1,14 @@
+var c= 10;
+let d= 12;
+let e= c+d;
+console.log(e);
+if(d>10){
+    e= 100;
+    c= 12.23;
+}
+randomVar= 25;
+console.log(randomVar);
+console.log(e);
+console.log(c);
+const z= 1000;
+//z= 1200; is not possible
