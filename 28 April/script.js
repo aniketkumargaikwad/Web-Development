00cf14a36a962d416665c2d4f91956a85d14ff39
Aniket Gaikwad <aniketg721@gmail.com>
2022-05-08 19:30:49 +0530

    string functions

---
+++ 28 April/script.js
@@ -0,0 +1,15 @@
+let a= "hello my name is \"Aniket\""
+let c= "hello my name is \"Aniket\""
+    //  12345678911234567 8921234 5
+console.log(a)
+console.log(a.slice(-5))
+console.log(a.replace("my",'his'))
+console.log(a.concat(" excellent", ' vey good'))
+let b= "vvdfbdndgn";
+console.log(b.padStart(74,"s"))
+console.log(b.padEnd(74,"s"))
+
+let arr1= a.split(" ")
+console.log(arr1)
+let arr2= a.split("")
+console.log(arr2)
