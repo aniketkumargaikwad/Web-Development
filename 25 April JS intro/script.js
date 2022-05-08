9775eddf6b37e5afb13e1ecebb1fbdda49034bc0
Aniket Gaikwad <aniketg721@gmail.com>
2022-05-08 09:02:46 +0530

    variables in js

--- 25 April JS intro/script.js
+++ 25 April JS intro/script.js
@@ -10,5 +10,20 @@ randomVar= 25;
 console.log(randomVar);
 console.log(e);
 console.log(c);
-const z= 1000;
+const x= 1000;
 //z= 1200; is not possible
+var tempvar1= "hello";
+console.log(tempvar1);
+document.write("hey");
+document.getElementById("div1").innerHTML= "<h1>Hey I'm Changed"
+
+{
+    //this is code block
+    let z= 123; console.log(z);
+    var y= 1234; console.log(y);
+    const p= 12345; console.log(p);
+    
+}
+//console.log(z);  //this will give undefined error
+console.log(y);   //this will not
+console.log(p);   //this will give undefined error
