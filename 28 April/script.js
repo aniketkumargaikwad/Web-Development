72bf2f54a6c306bfa880ed9efca48855881912cd
Aniket Gaikwad <aniketg721@gmail.com>
2022-05-09 09:59:16 +0530

    minor change1

--- 28 April/script.js
+++ 28 April/script.js
@@ -1,12 +1,12 @@
-let a= "hello my name is \"Aniket\""
-let c= "hello my name is \"Aniket\""
+let a= "hello my name is Aniket"
+let c= "hello my name is Aniket"
     //  12345678911234567 8921234 5
 console.log(a)
 console.log(a.slice(-5))
 console.log(a.replace("my",'his'))
 console.log(a.concat(" excellent", ' vey good'))
-let b= "vvdfbdndgn";
-console.log(b.padStart(74,"s"))
+let b= "v";
+console.log(b.padStart(7,"s"))
 console.log(b.padEnd(74,"s"))
 
 let arr1= a.split(" ")
