cf4eb31a9b902a46656494dd9626c5aa5ef9927c
Aniket Gaikwad <aniketg721@gmail.com>
2022-06-03 09:13:51 +0530

    ecom3

--- Projects/eCommerce/script.js
+++ Projects/eCommerce/script.js
@@ -13,3 +13,27 @@ if(close){
         nav.classList.remove('active1')
     })
 }
+
+const defText= document.getElementById("para");
+// const speed= document.getElementById("speed");
+
+const text=" Save more with coupons and get upto 70% off"
+
+let index= 1;
+const number= 500;
+// let time= number/speed.value
+let time= number/8
+
+function writeText(){
+    defText.innerHTML= text.slice(0, index);
+    index++;
+    if(index>text.length){
+        index=0;
+    }
+    setTimeout(writeText,time)
+}
+
+writeText();
+// speed.addEventListener("input",(event)=>{
+//     time=number/event.target.value
+// })
